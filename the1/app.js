96152ce8e120c081ce35870cd6f83df81258a5b7
awhitetail <100tuy@gmail.com>
2016-06-29 13:50:32 +0800

    注册用户后台存储

--- the1/app.js
+++ the1/app.js
@@ -5,6 +5,7 @@ var path = require('path')
 var mongoose = require('mongoose')
 var _ = require('underscore')
 var Movie = require('./models/movie')
+var User = require('./models/user')
 var port = process.env.PORT || 3000
 var app = express()
 
@@ -35,6 +36,43 @@ app.get('/', function(req, res){
 	})
 	
 })
+
+//signup
+app.post('/user/signup', function(req, res){
+	var _user = req.body.user
+	//  /user/signup/1111?userid=1112
+	//  req.query.userid  req.query.userid
+	//  /user/signup/:userid
+	//  req.params.userid  req.param('userid')
+	//  /user/signup/111?userid=112 {userid:113}
+	//  req.params.userid  优先路由111，再body里113，再query里112
+
+	User.findOne({name: _user.name}, function(err, user){
+		if (err) console.log(err)
+		if (user) {
+			return res.redirect('/')
+		}else {
+			var user = new User(_user)
+			user.save(function(err, user){
+				if (err) console.log(err)
+				res.redirect('/admin/userlist')
+			})
+		}
+	})
+})
+//userlist page
+app.get('/admin/userlist', function(req, res){
+
+	User.fetch(function(err, users){
+		if (err) { console.log(err) }
+		res.render('userlist', {
+			title: '用户列表页',
+			users: users
+		})
+	})
+	
+})
+
 
 //detail page
 app.get('/movie/:id', function(req, res){
